f336a5553230fb80ea8d9196f99c71021143dabc
Jeang <jeang@thphrans-MacBook-Pro.local>
2026-07-09 16:33:42 +0700

    ponytail-agy-linux

---
+++ README.md
@@ -0,0 +1,1 @@
+source <(curl -fsSL https://raw.githubusercontent.com/jeangtpr/config/main/ponytail-agy.sh)
---
+++ ponytail-agy.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://antigravity.google/cli/install.sh | bash
+
+agy plugin install https://github.com/DietrichGebert/ponytail
